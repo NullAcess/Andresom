df52d6567d9e135e915e73e4a758d6cd6a54f69d
NullAcess <153278494+NullAcess@users.noreply.github.com>
2026-06-23 15:35:26 +0300

    change knight damage to 35 by git test

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ class Program
         Console.OutputEncoding = System.Text.Encoding.UTF8;
         Console.InputEncoding = System.Text.Encoding.UTF8;
 
-        Weapon sword = new Sword(requirementEnergy: 20, DamageType.PhysicalResist, damage: 46);
+        Weapon sword = new Sword(requirementEnergy: 20, DamageType.PhysicalResist, damage: 35);
         Weapon magicStick = new MagicStick(requirementEnergy: 15, DamageType.MagicDamage, damage: 25);
         Weapon bow = new Bow(requirementEnergy: 10, DamageType.RangeDamage, damage: 15);
         Weapon nastyClub = new NastyClub(requirementEnergy: 30, DamageType.PhysicalResist, damage: 80);
